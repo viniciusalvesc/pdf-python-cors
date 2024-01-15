32413634ca6beecbdf8e00fb59c23d55c23f7d49
v.campello <vinicius.alves@finlytech.com>
2024-01-15 10:25:34 -0300

    TAX-13 - feita alteração para o fastapi.

--- app/api/routers/__init__.py
+++ app/api/routers/__init__.py
@@ -5,10 +5,10 @@ Data de desenvolvimento: 13/01/2024
 Descrição: Arquivo responsável pela instância principal do APIRouter.
 """
 from fastapi import APIRouter
-# from api.routers.users import users
+from app.api.routers.users import users
 from app.api.routers.auth import auth
 
-router = APIRouter(prefix="/api")
+router = APIRouter()
 
 router.include_router(auth.auth_router)
-# # router.include_router(users.router)
+router.include_router(users.users_router)
--- app/api/routers/users/users.py
+++ app/api/routers/users/users.py
@@ -1,38 +1,42 @@
-# """
-# Nome do arquivo: authRoute.py
-# Autor: Vinicius Alves Campello
-# Data de desenvolvimento: 09/01/2024
-# Descrição: Arquivo responsável pela rota de usuários.
-# """
-# from fastapi import APIRouter
-# from api.models.ft_user_address.UserAddress import UserAddress
-# from api.models.ft_user.User import User
-# from api.models.ft_user_info.UserInfo import UserInfo
-# from config.alchemy import db
+"""
+Nome do arquivo: authRoute.py
+Autor: Vinicius Alves Campello
+Data de desenvolvimento: 15/01/2024
+Descrição: Arquivo responsável pela rota de usuários.
+"""
+from fastapi import APIRouter, Depends, HTTPException
+from app.api.models.ft_user.User import User
+from app.api.models.ft_user_address.UserAddress import UserAddress
+from app.api.models.ft_user_info.UserInfo import UserInfo
+from sqlalchemy.orm import Session
+from app.db.sqlalchemy import get_db
 
-# bp = Blueprint('users', __name__, url_prefix='/users')
+users_router = APIRouter(prefix="/users")
 
-# @bp.route('/<email>', methods=['GET'])
-# def findOne(email):
-#     try:
-#         user = (
-#             db.session.query(User)
-#             .join(UserInfo, User.id == UserInfo.user_id)
-#             .join(UserAddress, User.id == UserAddress.user_id)
-#             .filter(User.email == email)
-#             .first()
-#         )
+@users_router.get('/{email}')
+async def findOne(email: str, db: Session = Depends(get_db)):
+    try:
+        user = (
+            db.query(User)
+            .join(UserInfo, User.id == UserInfo.user_id)
+            .join(UserAddress, User.id == UserAddress.user_id)
+            .filter(User.email == email)
+            .first()
+        )
+
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
         
-#         user.password = None
+        user.password = None
 
-#         data_response = {
-#             'msg':'Usuário encontrado',
-#             'data':{
-#                 'user': user.serialize()
-#             }
-#         }
+        data_response = {
+            'msg':'Usuário encontrado',
+            'data':{
+                'user': user
+            }
+        }
         
-#         return jsonify(data_response), 200
-#     except Exception as e:
-#         print(e)
-#         return jsonify({'error': f'Erro inesperado, tente novamente.\nDescrição: {e}'}), 500
+        return data_response
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=f'Erro inesperado, tente novamente.\nDescrição: {e}')
